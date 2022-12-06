4284a4dd4fe0bb010520d7effc8ad8c5e03f999f
Gonçalo Rabaça <goncalomrabaca@gmail.com>
2022-12-06 19:55:48 +0000

    Key and mouse tracking behaviours

--- Assets/Scripts/MouseMovingParticles.cs
+++ Assets/Scripts/MouseMovingParticles.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class MouseMovingParticles : MonoBehaviour
 {
     Vector3 particlePosition;
-    public float particleSpeed = 1f;
+    public float particleSpeed = 10f;
+
+    public Vector3 lastMousePos;
+    public Vector3 mouseVelocity;
+
+    public GameObject calm;
+    public GameObject steady;
+    public GameObject chaotic;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        mouseVelocity = Input.mousePosition - lastMousePos;
+        lastMousePos = Input.mousePosition;
+        //mouseVelocity.x = mouseVelocity.x / Screen.width;
+        //mouseVelocity.y = mouseVelocity.y / Screen.height;
+
+        /*
         particlePosition = Input.mousePosition;
         particlePosition.z = particleSpeed;
         transform.position = Camera.main.ScreenToWorldPoint(particlePosition);
+        */
+
+        print(mouseVelocity);
+
+        if (mouseVelocity.x > 200f || mouseVelocity.y > 200f)
+        {
+            particlePosition = Input.mousePosition;
+            particlePosition.z = particleSpeed;
+            chaotic.transform.position = Camera.main.ScreenToWorldPoint(particlePosition);
+        } 
+        else if (mouseVelocity.x > 100f || mouseVelocity.y > 100f)
+        {
+            particlePosition = Input.mousePosition;
+            particlePosition.z = particleSpeed;
+            steady.transform.position = Camera.main.ScreenToWorldPoint(particlePosition);
+        }
+        else if (mouseVelocity.x > 1f || mouseVelocity.y > 1f)
+        {
+            particlePosition = Input.mousePosition;
+            particlePosition.z = particleSpeed;
+            calm.transform.position = Camera.main.ScreenToWorldPoint(particlePosition);
+        }
     }
 }
